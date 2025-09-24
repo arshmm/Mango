e3b19e132a1d02ff6fcc5726db3c8291d6676b7e
Arsh Mansuri <m.arsh.mansuri@gmail.com>
2025-09-24 22:17:25 +0100

    Base service implemented (28)

---
+++ Mango.Web/Service/BaseService.cs
@@ -0,0 +1,88 @@
+﻿using System.Net;
+using System.Text;
+using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Newtonsoft.Json;
+using static Mango.Web.Models.Utility.SD;
+
+namespace Mango.Web.Service
+{
+    public class BaseService : IBaseService
+    {
+
+
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+        {
+            try
+            {
+
+                HttpClient client = _httpClientFactory.CreateClient("MangoApi");
+                HttpRequestMessage message = new();
+                message.Headers.Add("Accept", "application/json");
+                //token 
+
+                message.RequestUri = new Uri(requestDto.Url);
+                if (requestDto.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(requestDto.Data), Encoding.UTF8, "application/json");
+                }
+
+                HttpResponseMessage apiResponse = null;
+
+                switch (requestDto.ApiType)
+                {
+                    case ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                apiResponse = await client.SendAsync(message);
+
+                switch (apiResponse.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        return new() { IsSuccess = false, Message = "Not Found" };
+                        break;
+                    case HttpStatusCode.Forbidden:
+                        return new() { IsSuccess = false, Message = "Access Denied" };
+                        break;
+                    case HttpStatusCode.Unauthorized:
+                        return new() { IsSuccess = false, Message = "Unauthorized" };
+                        break;
+                    case HttpStatusCode.InternalServerError:
+                        return new() { IsSuccess = false, Message = "Internal Server Error" };
+                        break;
+                    default:
+                        var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                        var apiResponseDto = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+                        return apiResponseDto;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                var dto = new ResponseDto
+                {
+                    IsSuccess = false,
+                    Message = ex.Message,
+                };
+                return dto;
+            }
+
+        }
+    }
+}
