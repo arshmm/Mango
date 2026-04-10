e8a115b08bcaf0c9dc7ab24c35337ab88f4ff7f2
Arsh Mansuri <m.arsh.mansuri@gmail.com>
2026-04-10 16:26:37 +0100

    Token passing to the API from frontend implemented (70)

--- Mango.Web/Service/BaseService.cs
+++ Mango.Web/Service/BaseService.cs
@@ -12,11 +12,13 @@ namespace Mango.Web.Service
 
 
         private readonly IHttpClientFactory _httpClientFactory;
-        public BaseService(IHttpClientFactory httpClientFactory)
+        private readonly ITokenProvider _tokenProvider;
+        public BaseService(IHttpClientFactory httpClientFactory, ITokenProvider tokenProvider)
         {
             _httpClientFactory = httpClientFactory;
+            _tokenProvider = tokenProvider;
         }
-        public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+        public async Task<ResponseDto?> SendAsync(RequestDto requestDto, bool withBearer)
         {
             try
             {
@@ -24,7 +26,12 @@ namespace Mango.Web.Service
                 HttpClient client = _httpClientFactory.CreateClient("MangoApi");
                 HttpRequestMessage message = new();
                 message.Headers.Add("Accept", "application/json");
-                //token 
+                //token
+                if (withBearer)
+                {
+                    var token = _tokenProvider.GetToken();
+                    message.Headers.Add("Authorization", $"Bearer {token}");
+                }
 
                 message.RequestUri = new Uri(requestDto.Url);
                 if (requestDto.Data != null)
